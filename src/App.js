1acbf1e937e1c4b771428d1c0d8431aaf06b7f5f
bekasquid <chs271904@gmail.com>
2019-06-22 16:11:34 +0900

    Stats.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
   render() {
     return (
         <div className='scoreboard'>
-          <Header title='My Scoreboard' totalPlayers={this.state.players.length} />
+          <Header title='My Scoreboard' players={this.state.players} />
           { this.state.players.map(player =>
               <Player name={player.name}
                        id={player.id}
