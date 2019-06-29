8631c56cb9ac0adc9fac8a3a03c5602f42bbaf62
bekasquid <chs271904@gmail.com>
2019-06-29 17:56:00 +0900

    Route 설계

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Home extends React.Component {
+    // eslint-disable-next-line react/react-in-jsx-scope
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
---
+++ src/pages/Root.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Home} from "./Home";
+import {Heroes} from "./Heroes";
+import {Scoreboard} from "./Scoreboard";
+
+export class Root extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <p>공통메뉴 영역</p>
+                {/*네스티드 라우팅 영역 */}
+                <Route exact path="/" component={Home}></Route>
+                <Route path="/heroes" component={Heroes}></Route>
+                <Route path="/scoreboard" component={Scoreboard}></Route>
+            </BrowserRouter>
+        )
+    }
+}
