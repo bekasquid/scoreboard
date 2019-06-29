3fe02bc04667cc5b6ce89b7341b391ba17babb34
bekasquid <chs271904@gmail.com>
2019-06-29 10:04:15 +0900

    pure component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = (props) => (
-    <div className='player'>
-        <span className='player-name'>
-            <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-            {props.name}
-        </span>
-        <Counter id={props.id} score={props.score} changeScore={props.changeScore} />
-    </div>
-)
+export class Player extends  React.PureComponent {
+    render() {
+        return (
+            <div className='player'>
+                <span className='player-name'>
+                    <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
+                    {this.props.name}
+                </span>
+                <Counter id={this.props.id} score={this.props.score} changeScore={this.props.changeScore} />
+            </div>
+        )
+    }
+}
+
+// export const Player = (props) => {
+//     console.log(props.name, ' rendered');
+//     return (
+//         <div className='player'>
+//         <span className='player-name'>
+//             <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
+//             {props.name}
+//         </span>
+//             <Counter id={props.id} score={this.props.score} changeScore={props.changeScore} />
+//         </div>
+//     )
+// }
