0116d127b7bf4b4d9fc08b01e1b60e924b451deb
bekasquid <chs271904@gmail.com>
2019-06-29 10:21:56 +0900

    shouldComponent 라이프사이클 메서드를 활용해서 reconcilation을 결정

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,15 +3,24 @@ import {Counter} from "./Counter";
 
 export class Player extends  React.PureComponent {
     render() {
+        console.log(this.props.name, ' rendered');
         return (
-            <div className='player'>
-                <span className='player-name'>
-                    <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
+            <div className="player">
+                <span className="player-name">
+                  <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
                     {this.props.name}
                 </span>
-                <Counter id={this.props.id} score={this.props.score} changeScore={this.props.changeScore} />
+                <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore} />
             </div>
         )
+    }
+    componentWillReceiveProps(nextProps, nextContext) {
+        // console.log('componentWillReceiveProps', nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(nextProps, nextState);
+        return nextProps.score !== this.props.score;
     }
 }
 
