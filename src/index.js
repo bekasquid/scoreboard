8dcc2d9160fe668b332495de4d8b3dcb5bc62b81
bekasquid <chs271904@gmail.com>
2019-06-29 18:22:02 +0900

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import {Root} from './pages/Root';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 // ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,12 +3,13 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Heroes} from "./Heroes";
 import {Scoreboard} from "./Scoreboard";
+import {Menu} from "./menu";
 
 export class Root extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <p>공통메뉴 영역</p>
+                <Menu />
                 {/*네스티드 라우팅 영역 */}
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/heroes" component={Heroes}></Route>
