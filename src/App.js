8704b38707593f62f7bfaf9ac62fdd66174250b7
bekasquid <chs271904@gmail.com>
2019-06-29 15:55:15 +0900

    HEADER, APP component를 redux로 변경

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
-import {Header} from './components/Header';
 import {Player} from "./components/Player";
 import {AddPlayerForm} from "./components/AddPlayerForm";
+import {connect} from "react-redux";
 
 class App extends React.Component {
-  state = {
-    players: [
-      {name: 'LDK', id: 1, score: 0},
-      {name: 'HONG', id: 2, score: 0},
-      {name: 'KIM', id: 3, score: 0},
-      {name: 'PARK', id: 4, score: 0}
-    ]
-  }
-  maxId = this.state.players.length;
+  maxId = 4;
 
   handleRemovePlayer = (id) => {
-    console.log('remove player:', id);
+    console.log('remove player: ', id);
+
     this.setState(prevState => ({
       players: prevState.players.filter(item => item.id !== id)
     }))
   }
 
-  /*
-    * 스코어를 변경시키는 함수
-    * @param id : 플레이어 아이디
-    * @param delta 증가면 1 감소면 -1
-  */
+  /**
+   * 스코어를 변경시키는 함수
+   * @param id : 플레이어 아이디
+   * @param delta 증가면 1 감소면 -1
+   */
   handleChangeScore = (id, delta) => {
-      // console.log("change Score", id, delta);
-      this.setState(prevState => {
-        prevState.players.forEach(player => {
-          if (player.id === id) {
-            player.score += delta;
-          }
-        })
-        return {players: [...prevState.players]}
+    console.log('change score', id, delta);
+
+    this.setState(prevState => {
+      prevState.players.forEach(player => {
+        if (player.id === id) {
+          player.score += delta;
+        }
       })
+      return {players: [...prevState.players]}
+    })
   }
 
   handleAddPlayer = (name) => {
-    console.log('add player name:', name);
+    console.log('add player name: ', name);
     this.setState(prevState => {
       prevState.players.push({
         name,
         id: ++this.maxId,
         score: 0
-      })
+      });
       return {
         players: [...prevState.players]
       }
@@ -55,32 +50,28 @@ class App extends React.Component {
 
   render() {
     return (
-        <div className='scoreboard'>
-          <Header players={this.state.players} />
-          { this.state.players.map(player =>
-              <Player name={player.name}
-                       id={player.id}
-                       score={player.score}
-                       key={player.id}
-                       removePlayer={this.handleRemovePlayer}
-                       changeScore={this.handleChangeScore} />
-              ) }
-          <AddPlayerForm addPlayer={this.handleAddPlayer} />
+        <div className="scoreboard">
+          <Header players={this.props.players} />
+
+          {
+            this.props.players.map(player => (
+                <Player key={player.id}
+                        name={player.name} id={player.id} score={player.score}
+                        removePlayer={this.handleRemovePlayer}
+                        changeScore={this.handleChangeScore}/>
+            ))
+          }
+
+          <AddPlayerForm addPlayer={this.handleAddPlayer}/>
         </div>
     )
   }
 }
 
+const mapStateToProps = (state) => ({
+  // 왼쪽은 props, 오른쪽은 state
+  players: state.playerReducer.players
+})
 
-// function Header(){
-//     return (
-//         <header className='header'>
-//             <h1 className='h1'>ScoreBoard</h1>
-//             <span className='stats'>Players : 1</span>
-//         </header>
-//     )
-// }
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-export default App;
+// 커링 펑션, HoC
+export default connect(mapStateToProps, null)(App);
--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,3 +1,13 @@
-export const playerReducer = (state, action) => {
+const playerInitialState = {
+    title: 'My Scoreboard',
+    players: [
+        {name: 'LDK', id: 1, score: 0},
+        {name: 'HONG', id: 2, score: 0},
+        {name: 'KIM', id: 3, score: 0},
+        {name: 'PARK', id: 4, score: 0},
+    ]
+}
+
+export const playerReducer = (state = playerInitialState, action) => {
     return state;
 }
