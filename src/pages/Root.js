ff82a86428e074a4688aca76e4496e39ccd3d134
bekasquid <chs271904@gmail.com>
2019-07-06 18:18:58 +0900

    save

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
-import {Hero} from "./heroes/Hero";
 import {Heroes} from "./heroes/Heroes";
 import Scoreboard from "./scoreboard/Scoreboard";
 import {Menu} from "./menu";
+import {Hero} from "./heroes/Hero";
+import {Index} from "./heroes/Index";
 
 export class Root extends React.Component {
 
     render() {
         return (
             <BrowserRouter>
-                <Menu />
+                <Menu></Menu>
                 {/*네스티드 라우팅 영역*/}
-                <Switch>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route path="/heroes" component={Heroes}></Route>
-                    <Route path="/scoreboard" component={Scoreboard}></Route>
-                    <route path="/hero/:id" component={Hero}></route>
-                </Switch>
+                <div className="container" style={{backgroundColor: '#ffffff'}}>
+                    <Switch>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route path="/heroes" component={Index}></Route>
+                        <Route path="/scoreboard" component={Scoreboard}></Route>
+                    </Switch>
+                </div>
             </BrowserRouter>
         );
     }
